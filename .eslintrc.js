9ecf0a3714bcbf1ee0c4942ac7f6140b30d661a7
medalichakhari <medalichakhari.dev@gmail.com>
2025-11-01 15:41:01 +0100

    Add linting and start scripts to package.json; include ESLint as a dev dependency

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "commonjs",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-console": "off",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
